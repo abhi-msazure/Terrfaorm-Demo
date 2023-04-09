b45be8b75884fed3ab7d61d41d8c05407d29053b
abhi-msazure <127007419+abhi-msazure@users.noreply.github.com>
2023-04-09 12:23:50 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,21 +1,42 @@
-provider "azurerem" {
-version = "=2.4.0"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "Terraform_rg"
-  location = "West Europe"
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "TerraformStorageAccount"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_resource_group" "tfdemo" {
+  name     = "tfdemo"
+  location = "West US"
+}
+
+resource "azurerm_storage_account" "tfdemosa" {
+  name                     = "tfdemosa${random_string.resource_code.result}"
+  resource_group_name      = azurerm_resource_group.tfdemo.name
+  location                 = azurerm_resource_group.tfdemo.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
+  allow_blob_public_access = false
 
   tags = {
     environment = "dev"
   }
+}
+
+resource "azurerm_storage_container" "tfdemoscon" {
+  name                  = "tfdemoscon"
+  storage_account_name  = azurerm_storage_account.tfdemosa.name
+  container_access_type = "private"
 }
